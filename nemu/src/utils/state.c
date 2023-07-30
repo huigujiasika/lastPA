9ab3f7429096c0a2ada2744cf9ec168330999113
tracer-ics2022 <tracer@njuics.org>
2023-07-30 11:02:04 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-QFJQBA98 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:02:04 up  2:22,  0 users,  load average: 0.05, 0.01, 0.00

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -18,7 +18,9 @@
 NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
-  int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-    (nemu_state.state == NEMU_QUIT);
-  return good;
+  int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ;
+  //  ||  (nemu_state.state == NEMU_QUIT);
+  //  TODO:: 此处为了完美退出将其初始掉 
+  
+  return !good;
 }

83af46d4431c3d5a77bb961a35e3ec00aacd4bd5
tracer-ics2022 <tracer@njuics.org>
2023-07-30 12:00:40 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-QFJQBA98 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:00:40 up  3:20,  0 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -50,12 +50,20 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
-  set_quit();
+  set_quit();                //TODO:: 此处为了退出不报错加的，可能有问题
 
   return -1;
 }
 
 static int cmd_help(char *args);
+
+// static int cmd_si(char *args){
+//   char *args_steps = strtok(args, " ");
+//   int steps=atoi(args_steps);
+//   cpu_exec(steps);
+
+//   return 0;
+// }
 
 static struct {
   const char *name;
@@ -65,6 +73,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  //{ "si", "choose steps to excute proogram", cmd_si },
 
   /* TODO: Add more commands */
 

709e2c96429c6e09c1b9f137a9559dc432bb919a
tracer-ics2022 <tracer@njuics.org>
2023-07-30 16:26:15 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-QFJQBA98 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:26:15 up  7:46,  0 users,  load average: 0.09, 0.07, 0.01

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -106,7 +106,7 @@ void watch_display(){
 
       uint32_t  result=expr(findWp->exp,&success);
       findWp->result=result;
-      printf("\n%-4d %s %7x\n",findWp->NO,findWp->exp,findWp->result);
+      printf("\n%-4d %s %8x\n",findWp->NO,findWp->exp,findWp->result);
 
       findWp=findWp->next;
     }

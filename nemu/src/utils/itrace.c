9599131cf1c8b4de1099e3df041af56a8df228a3
tracer-ics2022 <tracer@njuics.org>
2023-07-31 17:11:50 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-QFJQBA98 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:11:50 up  8:11,  0 users,  load average: 0.18, 0.08, 0.02

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -74,12 +74,12 @@ void display_iringbuf(){         //iringbuf
 
 //Mtrace
 void read_mtrace(vaddr_t addr, int len,char* readmtrace_address){   
-    printf("READ at " FMT_PADDR " len=%d\n", addr, len);
+    //printf("READ at " FMT_PADDR " len=%d\n", addr, len);
 
-    // FILE* fp=fopen(readmtrace_address,"w+");
-    // fprintf(fp,"\n\n\nBEGIN MEMORy:\n");
-    // fprintf(fp,"pread at " FMT_PADDR " len=%d\n", addr, len);
-    // fclose(fp);
+    FILE* fp=fopen(readmtrace_address,"w+");
+    fprintf(fp,"\n\n\nBEGIN MEMORy:\n");
+    fprintf(fp,"pread at " FMT_PADDR " len=%d\n", addr, len);
+    fclose(fp);
 
 }
 
